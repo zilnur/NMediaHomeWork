7953eab30b0e6d289719666a20f52fd96d73cc1c
zilnur <86298777+zilnur@users.noreply.github.com>
2024-07-17 21:47:01 +0300

    Преобразовал метод конвертации чисел

--- app/src/main/java/ru/netology/nmedia/Post.kt
+++ app/src/main/java/ru/netology/nmedia/Post.kt
@@ -11,15 +11,33 @@ data class Post(
 )
 
 public fun Int.toPresentableString(): String {
-    val str = this.toString().removeRange(0,0)
-    return if (this > 1000) {
-        val suffix = if (this > 1000000) "M" else "K"
-        if (str[1] == '0') {
-            str[0].toString() + suffix
-        } else {
-            str[0].toString() + "." + str[1].toString() + suffix
+    return when {
+        this < 1_000 -> this.toString()
+        this in 1_000..99_999 -> {
+            val thousands = this / 1_000
+            val hundreds = (this % 1_000) / 100
+            if (hundreds == 0) {
+                "${thousands}K"
+            } else {
+                "${thousands}.${hundreds}K"
+            }
         }
-    } else {
-        this.toString()
+        this in 10_000..999_999 -> {
+            val thousands = this / 1_000
+            "${thousands}K"
+        }
+        this in 1_000_000..9_999_999 -> {
+            val millions = this / 1_000_000
+            val thousands = (this % 1_000_000) / 100_000
+            if (thousands == 0) {
+                "${millions}M"
+            } else {
+                "${millions}.${thousands}M"
+            }
+        }
+        else -> {
+            val millions = this / 1_000_000
+            "${millions}M"
+        }
     }
 }
